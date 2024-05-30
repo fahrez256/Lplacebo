e2699d2eda8e1050fc6a18bdaba117158a96a8a7
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-05-30 20:16:49 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ echo "Version : free/test"
 echo "Connecting..."
 sleep 5
 echo "Success"
-[ -n $runPackage] && echo "Applying for $runPackage" && flaunch $runPackage
+[ -n $runPackage ] && echo "Applying for $runPackage" && flaunch $runPackage
