4cffcecb2e2ec5e748e5d153a6bd8309d50cab29
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-05-30 18:40:03 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+source $FUNCTION
+source axeron.prop
+echo "$name | V1.0-Test | by FahrezONE"
+echo "Version : free/test"
+echo "Applying for $runPackage"
+echo "Connecting..."
+sleep 5
+echo "Success"
+flaunch $runPackage
